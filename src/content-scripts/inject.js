6be958ee662af38066cc3a69b3feae0a3db22a75
uluturki <uluturki@mail.usf.edu>
2019-06-23 18:02:57 -0400

    added a div to the fixed navmenu.

--- src/content-scripts/inject.js
+++ src/content-scripts/inject.js
@@ -1,1 +1,15 @@
 document.body.style.backgroundColor="orange";
+
+var survey = document.createElement('div');
+survey.className = "survey-container";
+survey.innerHTML = "Attention! Military software detected. Top secret clearance required.";
+
+var fixedBar = document.getElementsByClassName("global-nav-inner");
+// There is only one of these, if twitter adds another one, will have to reconsider.
+fixedBar[0].appendChild(survey);
+// var i;
+// for (i = 0; i < fixedBar.length; i++) {
+	// fixedBar[i].appendChild(survey);
+// }
+
+
