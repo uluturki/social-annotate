73e51eedfb14b2352544883a06249cde887fca5a
uluturki <uluturki@mail.usf.edu>
2019-07-15 18:04:16 -0400

    bug-fix: catch history state updates (unhandled)
    Catches history state updates, which do not trigger the content script.
    Event is not handled at this point, requires some work, see issue #11.

--- src/background/background.js
+++ src/background/background.js
@@ -6,22 +6,21 @@
 
 chrome.runtime.onInstalled.addListener(function() {
 	
-  let initialStorage = {
+	let initialStorage = {
 	  "resultsArray": [], 
 	  "annotatedUserIDs": [], 
 	  "config": config, 
 	  "isEnabled": true,
 	  "isGuided": false,
 	  "activeTargetList": [...config.surveys["twitter-user"].screenNameList]  // clone the array, keep the initial list for future reference.
-    // OV: Use options page to load this list manually
-  }
-  // {"resultsArray": [], "annotatedUserIDs": [], "config": config, "isEnabled": true}
-  chrome.storage.local.set(initialStorage, function() {
-    console.log('Storage arrays initialized.');
-  });
-  
-  
-  // chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+	// OV: Use options page to load this list manually
+	}
+	// {"resultsArray": [], "annotatedUserIDs": [], "config": config, "isEnabled": true}
+	chrome.storage.local.set(initialStorage, function() {
+	console.log('Storage arrays initialized.');
+	});
+	
+	// chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     // chrome.declarativeContent.onPageChanged.addRules([{
       // conditions: [new chrome.declarativeContent.PageStateMatcher({
         // pageUrl: {hostEquals: 'developer.chrome.com'},
@@ -30,3 +29,16 @@ chrome.runtime.onInstalled.addListener(function() {
     // }]);
   // });
 });
+
+
+// This is the event that catches HTML5 pushState page transitions:
+//	https://stackoverflow.com/questions/20865581/chrome-extension-content-script-not-loaded-until-page-is-refreshed
+chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
+    // @TODO: load necessary content scripts here, this is going to take some work to handle customizability properly
+	//	I will leave this out for now.
+	
+	// Possibly: This can send a message to the content script, which will then rerun itself/restart from checking.
+	
+	// chrome.tabs.executeScript(null,{file:"contentscript.js"});
+});
+  
